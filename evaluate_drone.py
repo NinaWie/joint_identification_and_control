corrected normalization by drone position

evaluate_drone.py
@@ -28,7 +28,7 @@ except ModuleNotFoundError:
 
 ROLL_OUT = 1
 ACTION_DIM = 4
-NR_REF_TO_INP = 3
+NR_REF_TO_INP = 1
 
 # Use cuda if available
 device = "cpu"  # torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -76,6 +76,8 @@ class QuadEvaluator():
         in_state, current_state, ref = self.dataset.get_and_add_eval_data(
             current_np_state.copy(), ref_states, add_to_dataset=add_to_dataset
         )
+        for m in range(ref.shape[1]):
+            ref[:, m, :3] = (ref[:, m, :3] - current_state[:, :3])
         # check if we want to train on this sample
         do_training = (
             (self.optimizer is not None)
@@ -227,7 +229,7 @@ class QuadEvaluator():
             trajectory = reference.get_ref_traj(current_np_state, acc)
             for k in range(7):
                 action = self.predict_actions(
-                    current_np_state, trajectory[k:k + 3]
+                    current_np_state, trajectory[k:k + NR_REF_TO_INP]
                 )
                 # only use first action (as in mpc)
                 current_np_state, stable = self.eval_env.step(

neural_control/dataset.py
@@ -172,17 +172,15 @@ class DroneDataset(torch.utils.data.Dataset):
 
         # 1) just output the unnormalized states, but with the position at zero
         drone_states = self.to_torch(states)
-        drone_pos = drone_states[:, :3].clone()
-        drone_states[:, :3] = 0
+        # drone_pos = drone_states[:, :3].clone()
+        # drone_states[:, :3] = 0
 
         # 2) Normalized states
         normed_drone_states = self.normalize_states(drone_states.clone())
 
         # 3) Reference trajectory to torch and relative to drone position
         torch_ref_states = self.to_torch(ref_states)
-        for i in range(ref_states.shape[1]):
-            torch_ref_states[:,
-                             i, :3] = (torch_ref_states[:, i, :3] - drone_pos)
+
         # transform acceleration
         torch_ref_states[:, :, 6:] *= self.kwargs["dt"]
 

train_drone.py
@@ -26,7 +26,7 @@ NR_EVAL_ITERS = 5
 STATE_SIZE = 12
 NR_ACTIONS_REF = 10
 NR_ACTIONS_PREDICT = 1
-NR_REF_TO_INP = 3
+NR_REF_TO_INP = 1
 NR_ACTIONS_LOSS = NR_ACTIONS_REF - NR_REF_TO_INP
 REF_DIM = 9
 ACTION_DIM = 4
@@ -175,8 +175,18 @@ for epoch in range(NR_EPOCHS):
                 in_state.size()[0], NR_ACTIONS_LOSS, STATE_SIZE
             )
             for k in range(NR_ACTIONS_LOSS):
+                # prepare ref
+                in_ref_pos = torch.zeros(in_state.size()[0], NR_REF_TO_INP, 3)
+                for m in range(NR_REF_TO_INP):
+                    in_ref_pos[:, m] = ref_states[:, k +
+                                                  m, :3] - current_state[:, :3]
+                in_ref_vel = ref_states[:, k:k + NR_REF_TO_INP, 3:]
+                in_ref_state = torch.cat((in_ref_pos, in_ref_vel), dim=2)
+
                 in_state = state_data.normalize_states(current_state)
-                actions = net(in_state, ref_states[:, k:k + NR_REF_TO_INP])
+
+                # action
+                actions = net(in_state, in_ref_state)
                 action = torch.sigmoid(actions)
 
                 current_state = simulate_quadrotor(