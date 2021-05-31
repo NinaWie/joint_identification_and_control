e412a59a5292fb543ca64f7c1ba6ea28c4d4f6b7
NinaWie <wnina@student.ethz.ch>
2021-05-31 22:44:59 +0200

    refactor: train and eval into script

--- run_eval.sh
+++
@@ -1,5 +0,0 @@
-python evaluate_fixed_wing.py -m finetune_seq_con_pretrained_200 -a 50
-python evaluate_fixed_wing.py -m finetune_seq_con_pretrained_400 -a 50
-python evaluate_fixed_wing.py -m finetune_seq_con_pretrained_800 -a 50
-python evaluate_fixed_wing.py -m finetune_seq_con_pretrained_1200 -a 50
-python evaluate_fixed_wing.py -m finetune_seq_con_pretrained_1800 -a 50
